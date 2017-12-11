4491cb8df3f0cdaf2f6f713e6c440d9d2e0b2543
Sheila Rezkia <sheilarezkia28@gmail.com>
2017-12-11 11:53:07 +0700

    added fitur 1 dummy template, fixed variable naming in views. and added a few test

--- fitur_1/tests.py
+++ fitur_1/tests.py
@@ -5,10 +5,18 @@ from django.urls import resolve
 # Create your tests here.
 
 class UnitTest(TestCase):
-    def test_fitur_1_url_exists(self):
-        response = Client().get('/fitur-1/')
-        self.assertEqual(response.status_code, 200)
+	def test_fitur_1_url_exists(self):
+		response = Client().get('/fitur-1/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_redirect_landing_page_to_login_page(self):
-    	response= Client().get('/')
-    	self.assertEqual(response.status_code, 302)
+	def test_redirect_landing_page_to_login_page(self):
+		response= Client().get('/')
+		self.assertEqual(response.status_code, 302)
+
+	def test_landing_page(self):
+		#go to log in page if user is not logged in
+		response = self.client.get('/fitur-1/')
+		html_response = response.content.decode('utf-8')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed('fitur_1/fitur_1.html')
+		self.assertIn("Login with LinkedIn", html_response)
--- fitur_1/views.py
+++ fitur_1/views.py
@@ -6,6 +6,6 @@ import json
 response = {}
 
 def index(request):
-    response['author'] = 'Sheila Rezkia'
-    html = 'fitur_1/fitur_1.html'
-    return render(request, html, response)
+	response['author'] = 'Sheila Rezkia'
+	html1 = 'fitur_1/fitur_1.html'
+	return render(request, html1, response)
