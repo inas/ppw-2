42ad18e0a31f892554e3239c370d205b2f645a99
Sheila Rezkia <sheilarezkia28@gmail.com>
2017-12-11 11:26:27 +0700

    added test and a function to fitur_1

--- fitur_1/tests.py
+++ fitur_1/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
 
 # Create your tests here.
+
+class UnitTest(TestCase):
+    def test_fitur_1_url_exists(self):
+        response = Client().get('/fitur-1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_redirect_landing_page_to_login_page(self):
+    	response= Client().get('/')
+    	self.assertEqual(response.status_code, 302)
--- fitur_1/views.py
+++ fitur_1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+import os
+import json
 
-# Create your views here.
+response = {}
+
+def index(request):
+    response['author'] = 'Sheila Rezkia'
+    html = 'fitur_1/fitur_1.html'
+    return render(request, html, response)
