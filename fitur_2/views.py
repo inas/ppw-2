97fd82998e0603c4341323faca4ecbd1e43ebf74
anishainas <anishainas@gmail.com>
2017-12-13 07:17:54 +0700

    ???

--- fitur_2/views.py
+++ fitur_2/views.py
@@ -7,14 +7,25 @@ import json
 response = {}
 
 def index(request):
-    response['author'] = 'Anisha Inas'
-    html = 'fitur_2/fitur_2.html'
-    print ("#==> profile")
-    # ## sol : bagaimana cara mencegah error, jika url profile langsung diakses
-    # if 'user_login' not in request.session.keys():
-    #     return HttpResponseRedirect(reverse('fitur-1:index'))
-    # ## end of sol
+	response['author'] = 'Anisha Inas'
+	html = 'fitur_2/fitur_2.html'
+	print ("#==> profile")
+	# ## sol : bagaimana cara mencegah error, jika url profile langsung diakses
+	# if 'user_login' not in request.session.keys():
+	#     return HttpResponseRedirect(reverse('fitur-1:index'))
+	# ## end of sol
 
-    # set_data_for_session(response, request)
+	# set_data_for_session(response, request)
+	print('user_login' in request.session)
+	if('user_login' in request.session):
+		print("udah login")
+		try:
+			response['name'] = request.session.get('name')
+			if(response['name']==None):
+				raise KeyError
+		except KeyError:
+			get_data(request)
 
-    return render(request, html, response)
+		return render(request, html, response)
+	else:
+		return HttpResponseRedirect('../fitur-1/')
