535af46a517682eb0c4077bfa064904f50cfc359
anishainas <anishainas@gmail.com>
2017-12-18 20:12:53 +0700

    deleted test

--- fitur_2/tests.py
+++ fitur_2/tests.py
@@ -3,8 +3,3 @@ from django.test import Client
 from django.urls import resolve
 
 # Create your tests here.
-
-class fitur2UnitTest(TestCase):
-    def test_fitur_2_url_is_exist(self):
-        response = Client().get('/fitur-2/')
-        self.assertEqual(response.status_code, 200)
